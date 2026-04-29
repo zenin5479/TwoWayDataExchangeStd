77c6f9263b4a83cecb8e9f5f78d0ce408ed181ab
zenin5479 <zenin5479@gmail.com>
2026-04-29 17:35:30 +0300

    Update 0.7 Обновлен метод. Проведен рефакторинг

--- WinFormsAppOne/Form1.Designer.cs
+++ WinFormsAppOne/Form1.Designer.cs
@@ -34,7 +34,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(384, 450);
+         ClientSize = new System.Drawing.Size(384, 261);
          Name = "Form1";
          Text = "Form1";
          ResumeLayout(false);
