f0bfb859d9871f9c6c3b0e29e62e18c799a7f4e4
zenin5479 <zenin5479@gmail.com>
2026-04-30 12:58:03 +0300

    Update 3.5 Обновлен метод. Проведен рефакторинг

--- WinFormsAppTwo/FormTwo.Designer.cs
+++ WinFormsAppTwo/FormTwo.Designer.cs
@@ -28,19 +28,67 @@
       /// </summary>
       private void InitializeComponent()
       {
+         LabelPrompt = new System.Windows.Forms.Label();
+         ButtonCalculator = new System.Windows.Forms.Button();
+         LabelResult = new System.Windows.Forms.Label();
+         TextBoxNumber = new System.Windows.Forms.TextBox();
          SuspendLayout();
+         // 
+         // LabelPrompt
+         // 
+         LabelPrompt.AutoSize = true;
+         LabelPrompt.Location = new System.Drawing.Point(112, 86);
+         LabelPrompt.Name = "LabelPrompt";
+         LabelPrompt.Size = new System.Drawing.Size(161, 15);
+         LabelPrompt.TabIndex = 7;
+         LabelPrompt.Text = "Введите целое число (0..20):";
+         // 
+         // ButtonCalculator
+         // 
+         ButtonCalculator.Location = new System.Drawing.Point(112, 133);
+         ButtonCalculator.Name = "ButtonCalculator";
+         ButtonCalculator.Size = new System.Drawing.Size(150, 23);
+         ButtonCalculator.TabIndex = 6;
+         ButtonCalculator.Text = "Вычислить факториал";
+         ButtonCalculator.UseVisualStyleBackColor = true;
+         // 
+         // LabelResult
+         // 
+         LabelResult.AutoSize = true;
+         LabelResult.Location = new System.Drawing.Point(112, 159);
+         LabelResult.Name = "LabelResult";
+         LabelResult.Size = new System.Drawing.Size(66, 15);
+         LabelResult.TabIndex = 5;
+         LabelResult.Text = "Результат: ";
+         // 
+         // TextBoxNumber
+         // 
+         TextBoxNumber.Location = new System.Drawing.Point(112, 104);
+         TextBoxNumber.Name = "TextBoxNumber";
+         TextBoxNumber.Size = new System.Drawing.Size(150, 23);
+         TextBoxNumber.TabIndex = 4;
          // 
          // FormTwo
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(384, 261);
+         Controls.Add(LabelPrompt);
+         Controls.Add(ButtonCalculator);
+         Controls.Add(LabelResult);
+         Controls.Add(TextBoxNumber);
          Name = "FormTwo";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Факториал через консоль";
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.Label LabelPrompt;
+      private System.Windows.Forms.Button ButtonCalculator;
+      private System.Windows.Forms.Label LabelResult;
+      private System.Windows.Forms.TextBox TextBoxNumber;
    }
 }
