3337e17083ad98b454a2e0139b5fed85cd67c267
zenin5479 <zenin5479@gmail.com>
2026-04-30 13:00:38 +0300

    Update 3.7 Обновлен метод. Проведен рефакторинг

--- WinFormsAppTwo/FormTwo.cs
+++ WinFormsAppTwo/FormTwo.cs
@@ -1,9 +1,16 @@
-﻿using System.Windows.Forms;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace WinFormsAppTwo
 {
    public partial class FormTwo : Form
    {
+      [DllImport("kernel32.dll")]
+      private static extern bool AllocConsole();
+
+      [DllImport("kernel32.dll")]
+      private static extern bool FreeConsole();
+
       public FormTwo()
       {
          InitializeComponent();
