fe69cf37d6489af33402f2db2173d93b788c8c7e
zenin5479 <zenin5479@gmail.com>
2026-05-02 11:54:05 +0300

    Update 5.5 Обновлен метод. Проведен рефакторинг

--- WinFormsAppThree/FormThree.Designer.cs
+++ WinFormsAppThree/FormThree.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsAppThree
 {
-   partial class Form1
+   partial class FormThree
    {
       /// <summary>
       ///  Required designer variable.
--- WinFormsAppThree/FormThree.cs
+++ WinFormsAppThree/FormThree.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinFormsAppThree
 {
-   public partial class Form1 : Form
+   public partial class FormThree : Form
    {
-      public Form1()
+      public FormThree()
       {
          InitializeComponent();
       }
