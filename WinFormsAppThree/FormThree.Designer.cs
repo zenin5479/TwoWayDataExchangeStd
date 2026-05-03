cb598642e0fd243b7382250aca873ce79ec30aa0
zenin5479 <zenin5479@gmail.com>
2026-05-03 11:01:41 +0300

    Update 6.7 Обновлен метод. Проведен рефакторинг

--- WinFormsAppThree/FormThree.Designer.cs
+++ WinFormsAppThree/FormThree.Designer.cs
@@ -28,13 +28,46 @@
       /// </summary>
       private void InitializeComponent()
       {
+         btnShowSelected = new System.Windows.Forms.Button();
+         listBoxPeople = new System.Windows.Forms.ListBox();
+         btnStartConsole = new System.Windows.Forms.Button();
          SuspendLayout();
+         // 
+         // btnShowSelected
+         // 
+         btnShowSelected.Location = new System.Drawing.Point(12, 141);
+         btnShowSelected.Name = "btnShowSelected";
+         btnShowSelected.Size = new System.Drawing.Size(75, 23);
+         btnShowSelected.TabIndex = 5;
+         btnShowSelected.Text = "Показать выбранное";
+         btnShowSelected.UseVisualStyleBackColor = true;
+         // 
+         // listBoxPeople
+         // 
+         listBoxPeople.FormattingEnabled = true;
+         listBoxPeople.ItemHeight = 15;
+         listBoxPeople.Location = new System.Drawing.Point(12, 41);
+         listBoxPeople.Name = "listBoxPeople";
+         listBoxPeople.Size = new System.Drawing.Size(268, 94);
+         listBoxPeople.TabIndex = 4;
+         // 
+         // btnStartConsole
+         // 
+         btnStartConsole.Location = new System.Drawing.Point(12, 12);
+         btnStartConsole.Name = "btnStartConsole";
+         btnStartConsole.Size = new System.Drawing.Size(130, 23);
+         btnStartConsole.TabIndex = 3;
+         btnStartConsole.Text = "Запустить консоль";
+         btnStartConsole.UseVisualStyleBackColor = true;
          // 
          // FormThree
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(384, 261);
+         Controls.Add(btnShowSelected);
+         Controls.Add(listBoxPeople);
+         Controls.Add(btnStartConsole);
          MaximizeBox = false;
          MinimizeBox = false;
          Name = "FormThree";
@@ -44,5 +77,9 @@
       }
 
       #endregion
+
+      private System.Windows.Forms.Button btnShowSelected;
+      private System.Windows.Forms.ListBox listBoxPeople;
+      private System.Windows.Forms.Button btnStartConsole;
    }
 }
