f8b140649bf98022251895a754e9bd55cd7211e4
zenin5479 <zenin5479@gmail.com>
2026-05-03 11:15:13 +0300

    Update 7.6 Обновлен метод. Проведен рефакторинг

--- WinFormsAppThree/ConsoleAppThree.cs
+++ WinFormsAppThree/ConsoleAppThree.cs
@@ -1,6 +1,58 @@
-﻿namespace WinFormsAppThree
+﻿using System;
+
+namespace WinFormsAppThree
 {
    internal class ConsoleAppThree
    {
+      private DataStorage _storage;
+
+      public ConsoleAppThree(DataStorage storage)
+      {
+         _storage = storage;
+      }
+
+      public void Run()
+      {
+         Console.WriteLine("Консольное приложение запущено. Введите данные нового человека:");
+
+         while (true)
+         {
+            Console.Write("Имя (или 'ex' для выхода): ");
+            string name = Console.ReadLine();
+
+            if (name != null && name.ToLower() == "ex")
+            {
+               break;
+            }
+
+            Console.Write("Возраст: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Город: ");
+            string city = Console.ReadLine();
+            Console.Write("Улица: ");
+            string street = Console.ReadLine();
+            Console.Write("Почтовый индекс: ");
+            string postalCode = Console.ReadLine();
+
+            var address = new Address
+            {
+               City = city,
+               Street = street,
+               PostalCode = postalCode
+            };
+
+            var person = new Person
+            {
+               Name = name,
+               Age = age,
+               Address = address
+            };
+
+            _storage.AddPerson(person);
+            Console.WriteLine($"Человек '{name}' добавлен в хранилище.\n");
+         }
+      }
+
    }
 }
