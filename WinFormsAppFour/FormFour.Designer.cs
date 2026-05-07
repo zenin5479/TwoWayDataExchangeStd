9e41265d66df1e51ccb65f7038545ec0832e8917
zenin5479 <zenin5479@gmail.com>
2026-05-07 22:01:22 +0300

    Update 17.3 Обновлен метод. Проведен рефакторинг

--- WinFormsAppFour/FormFour.Designer.cs
+++ WinFormsAppFour/FormFour.Designer.cs
@@ -30,6 +30,8 @@
       {
          panel1 = new System.Windows.Forms.Panel();
          panel2 = new System.Windows.Forms.Panel();
+         dataGridView1 = new System.Windows.Forms.DataGridView();
+         ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
          SuspendLayout();
          // 
          // panel1
@@ -41,20 +43,31 @@
          // 
          // panel2
          // 
-         panel2.Location = new System.Drawing.Point(379, 278);
+         panel2.Location = new System.Drawing.Point(250, 12);
          panel2.Name = "panel2";
          panel2.Size = new System.Drawing.Size(200, 100);
          panel2.TabIndex = 1;
+         // 
+         // dataGridView1
+         // 
+         dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         dataGridView1.Location = new System.Drawing.Point(59, 202);
+         dataGridView1.Name = "dataGridView1";
+         dataGridView1.RowTemplate.Height = 25;
+         dataGridView1.Size = new System.Drawing.Size(240, 150);
+         dataGridView1.TabIndex = 2;
          // 
          // FormFour
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(768, 471);
+         Controls.Add(dataGridView1);
          Controls.Add(panel2);
          Controls.Add(panel1);
          Name = "FormFour";
          Text = "Система обмена данными с консолью";
+         ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
          ResumeLayout(false);
       }
 
@@ -62,5 +75,6 @@
 
       private System.Windows.Forms.Panel panel1;
       private System.Windows.Forms.Panel panel2;
+      private System.Windows.Forms.DataGridView dataGridView1;
    }
 }
