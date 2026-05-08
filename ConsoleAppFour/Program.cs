0b3607f1d558fc3725675415ff63321cea5ca3e2
zenin5479 <zenin5479@gmail.com>
2026-05-08 13:16:34 +0300

    Update 20.9 Обновлен метод. Проведен рефакторинг

--- ConsoleAppFour/Program.cs
+++ ConsoleAppFour/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleAppFour
    {
       static void Main()
       {
-         Console.WriteLine("КОНСОЛЬНЫЙ ОБРАБОТЧИК");
+         Console.WriteLine("Консольный ОБРАБОТЧИК");
          Console.WriteLine("Текущее количество людей в хранилище: {0}", GlobalStorage.TotalPeopleCount);
 
          // Показываем существующих людей
