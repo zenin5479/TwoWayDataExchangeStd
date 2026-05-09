7a94e9a2f69ce6c969169c38b205224f382226e3
zenin5479 <zenin5479@gmail.com>
2026-05-09 11:44:16 +0300

    Update 23.8 Обновлен метод. Проведен рефакторинг

--- WinFormsAppFour/FormFour.Designer.cs
+++ WinFormsAppFour/FormFour.Designer.cs
@@ -103,9 +103,9 @@
          label1.AutoSize = true;
          label1.Location = new System.Drawing.Point(461, 240);
          label1.Name = "label1";
-         label1.Size = new System.Drawing.Size(38, 15);
+         label1.Size = new System.Drawing.Size(89, 15);
          label1.TabIndex = 7;
-         label1.Text = "label1";
+         label1.Text = "Всего людей: 0";
          // 
          // FormFour
          // 
