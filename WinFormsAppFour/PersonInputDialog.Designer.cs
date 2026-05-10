ea1f174bb8fec989b66806b2fcf124e5d8829f33
zenin5479 <zenin5479@gmail.com>
2026-05-10 09:22:56 +0300

    Update 26.8 Обновлен метод. Проведен рефакторинг

--- WinFormsAppFour/PersonInputDialog.Designer.cs
+++ WinFormsAppFour/PersonInputDialog.Designer.cs
@@ -44,9 +44,9 @@
          label1.AutoSize = true;
          label1.Location = new System.Drawing.Point(57, 89);
          label1.Name = "label1";
-         label1.Size = new System.Drawing.Size(38, 15);
+         label1.Size = new System.Drawing.Size(34, 15);
          label1.TabIndex = 1;
-         label1.Text = "label1";
+         label1.Text = "Имя:";
          // 
          // PersonInputDialog
          // 
