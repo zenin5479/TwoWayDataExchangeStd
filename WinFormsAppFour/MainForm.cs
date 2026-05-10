ef0096946c2876429b2824bffaa20aa5888dfc11
zenin5479 <zenin5479@gmail.com>
2026-05-10 16:31:13 +0300

    Update 31.2 Обновлен метод. Проведен рефакторинг

--- WinFormsAppFour/MainForm.cs
+++ WinFormsAppFour/MainForm.cs
@@ -33,6 +33,39 @@ namespace WinFormsAppFour
 
       private void btnRunConsole_Click(object sender, EventArgs e)
       {
+
+         // Абсолютный путь к файлу
+         Console.WriteLine("Абсолютный путь к файлу в console application");
+         // 1 вариант 
+         string path1 = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+         Console.WriteLine(path1);
+         // 2 вариант 
+         string path2 = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+         Console.WriteLine(path2);
+         // 3 вариант 
+         ProcessModule path3 = Process.GetCurrentProcess().MainModule;
+         if (path3 != null)
+         {
+            string path30 = Path.GetDirectoryName(path3.FileName);
+            Console.WriteLine(path30);
+         }
+         // 4 вариант 
+         string path4 = Directory.GetCurrentDirectory();
+         Console.WriteLine(path4);
+         // 5 вариант 
+         string path5 = AppContext.BaseDirectory;
+         Console.WriteLine(path5);
+         // 6 вариант 
+         string path6 = Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory);
+         Console.WriteLine(path6);
+         // 7 вариант 
+         if (AppDomain.CurrentDomain.BaseDirectory != null)
+         {
+            string path7 = Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine(path7);
+         }
+
+
          // Находим путь к консольному EXE
          string consolePath = Path.Combine(
             Path.GetDirectoryName(Application.ExecutablePath),
