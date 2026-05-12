5c360111a492f8e38e689b8db8b0cff3cdbbff5a
zenin5479 <zenin5479@gmail.com>
2026-05-12 18:42:13 +0300

    Update 34.5 Обновлен метод. Проведен рефакторинг

--- WinFormsAppFour/PersonEditDialog.Designer.cs
+++ WinFormsAppFour/PersonEditDialog.Designer.cs
@@ -142,6 +142,7 @@
          Controls.Add(label1);
          Controls.Add(dtpBirth);
          Name = "PersonEditDialog";
+         StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Редактирование человека";
          ((System.ComponentModel.ISupportInitialize)numSalary).EndInit();
          ResumeLayout(false);
