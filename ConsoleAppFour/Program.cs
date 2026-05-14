f200bf03dbf71e4f32153035c427d875c0684313
zenin5479 <zenin5479@gmail.com>
2026-05-14 10:06:10 +0300

    Update 35.8 Обновлен метод. Проведен рефакторинг

--- ConsoleAppFour/Program.cs
+++ ConsoleAppFour/Program.cs
@@ -13,7 +13,6 @@ namespace ConsoleAppFour
          // Показываем существующих людей
          for (int index = 0; index < GlobalStorage.AllPeople.Count;)
          {
-            var person1 = GlobalStorage.AllPeople[index];
             Console.WriteLine("\nСуществующие люди:");
             int i = 0;
             while (i < GlobalStorage.AllPeople.Count)
@@ -22,8 +21,6 @@ namespace ConsoleAppFour
                Console.WriteLine("  {0}", person);
                i++;
             }
-
-
 
             break;
          }
